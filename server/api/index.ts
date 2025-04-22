b3c9170dbb3b84662da9a690a324d1d7450dddd5
Thinker-08 <agarwalmihir08@gmail.com>
2025-04-23 02:55:01 +0530

    Updated the routes name

--- server/api/index.ts
+++ server/api/index.ts
@@ -15,7 +15,7 @@ async function getApp() {
   app.use(
     cors({
       origin: "*",
-      methods: ["GET", "POST", "PUT", "DELETE"],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
     })

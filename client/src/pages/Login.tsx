5f71719712ffae0301daf31acfd5a267b5ad65a0
Thinker-08 <agarwalmihir08@gmail.com>
2025-04-24 00:12:02 +0530

    FE Changes

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { apiRequest } from "../lib/queryClient";
 import { useToast } from "../hooks/use-toast";
+import { useLocation } from "wouter";
 
 const extractErrorMessage = (error: any) => {
     try {
@@ -18,6 +19,7 @@ const extractErrorMessage = (error: any) => {
 
 export default function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
+  const [, setLocation] = useLocation();
   const { toast } = useToast();
 
   useEffect(() => {
@@ -25,7 +27,7 @@ export default function AuthForm() {
 
     // Redirect to home if the token exists
     if (authToken) {
-      window.location.href = "/home";
+      setLocation("/home");
     }
   }, []);
 
